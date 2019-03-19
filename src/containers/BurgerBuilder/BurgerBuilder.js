391801590b6dfb1b22b5441c7554224e0f655b60
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-03-20 02:03:19 +0530

    Project setup

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Auxiliary>
+                <div>
+                    Burger
+                </div>
+                <div>
+                    Burger Ingredients
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
