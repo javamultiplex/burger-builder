a4b057daf14fe0efef0f1fdb355116d02705c646
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-03-20 02:57:22 +0530

    Added more components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
-class BurgerBuilder extends Component{
-    render(){
-        return(
+class BurgerBuilder extends Component {
+    render() {
+        return (
             <Auxiliary>
-                <div>
-                    Burger
-                </div>
+                <Burger />
                 <div>
                     Burger Ingredients
                 </div>
